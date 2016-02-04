c7f40eaf23464dd531bb2d47ef1288fb2bcf5742
David Parry <drjparry@gmail.com>
2016-02-04 19:03:19 +0100

    did challenge 14

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -2,6 +2,7 @@ require 'selenium-webdriver'
 require 'capybara/dsl'
 
 feature "User Story => Battle" do
+  subject(:player){described_class.new}
 
   def firefox
     include Capybara::DSL
@@ -26,5 +27,19 @@ feature "User Story => Battle" do
     click_link('Attack')
     expect(page).to have_content("Dave attacked Barry")
   end
+
+  # As Player 1,
+  # So I can start to win a game of Battle,
+  # I want my attack to reduce Player 2's HP
+
+  scenario 'allows an attack to reduce players HP' do
+    sign_in_and_play
+    click_link('Attack')
+    click_link('Return to play')
+    expect(page).not_to have_content("Barry: 60 HP")
+    expect(page).to have_content("Barry: 50 HP")
+
+  end
+
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,12 +1,38 @@
 require 'player'
 
-feature Player do
+describe Player do
+  subject(:dave) {Player.new('Dave')}
+  subject(:barry) {Player.new('Barry')}
 
   # As Player 1,
   # So I can start to win a game of Battle,
   # I want my attack to reduce Player 2's HP
-  scenario 'it reduces player 2s hitpoints' do
-    sign_in_and_play
-    expect($player_1.name).to eq 'Dave'
+
+    describe '#name' do
+    it 'it reduces player 2s hitpoints' do
+    expect(dave.name).to eq 'Dave'
+    end
   end
+
+  describe '#hitpoints' do
+    it 'returns the hit points' do
+      expect(dave.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+  end
+
+  describe '#attack' do
+    it 'damages the player' do
+      expect(barry).to receive(:receive_damage)
+      dave.attack(barry)
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'reduces the player hit points' do
+      expect {dave.receive_damage }.to change { dave.hit_points }.by(-10)
+    end
+  end
+
+
+
 end
