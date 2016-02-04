216ea6edf83ff7e2c52d38d7af4742406363bdfe
David Parry <drjparry@gmail.com>
2016-02-04 13:57:21 +0100

    added git points

---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,18 @@
+require 'selenium-webdriver'
+require 'capybara/dsl'
+
+feature "User Story => Battle" do
+
+  def firefox
+    include Capybara::DSL
+    Capybara.default_driver = :selenium
+  end
+
+  # As two Players,
+  # So we can play a personalised game of Battle,
+  # We want to Start a fight by entering our names and seeing them
+  scenario "allow players entering their names" do
+    sign_in_and_play
+    expect(page).to have_content("David vs. Barry")
+  end
+end
---
+++ spec/features/hit_points_spec.rb
@@ -0,0 +1,30 @@
+require 'selenium-webdriver'
+require 'capybara/dsl'
+
+feature "User Story => Battle" do
+
+  def firefox
+    include Capybara::DSL
+    Capybara.default_driver = :selenium
+  end
+
+  # As Player 1,
+  # So I can see how close I am to winning
+  # I want to see Player 2's Hit Points
+  scenario "allows user to see player 2's hitpoints" do
+    # firefox
+    sign_in_and_play
+    expect(page).to have_content("Barry: 60/60 HP")
+  end
+
+# As Player 1,
+# So I can win a game of Battle,
+# I want to attack Player 2, and I want to get a confirmation
+
+  scenario 'allow user to see confirmation of attack' do
+    sign_in_and_play
+    click_button('Attack')
+    expect(page).to have_content("Barry: 50/60 HP")
+  end
+
+end
