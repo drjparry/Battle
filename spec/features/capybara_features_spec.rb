a1981babb965437f4fa52b7f5a8526ba72483168
barrygrubb <barrygrubb@users.noreply.github.com>
2016-02-04 12:07:07 +0000

    started implementing User Story 2

--- spec/features/capybara_features_spec.rb
+++ spec/features/capybara_features_spec.rb
@@ -1,12 +1,34 @@
+require 'selenium-webdriver'
+require 'capybara/dsl'
+
 feature "User Story => Battle" do
+
+  def firefox
+    include Capybara::DSL
+    Capybara.default_driver = :selenium
+  end
+
   # As two Players,
   # So we can play a personalised game of Battle,
   # We want to Start a fight by entering our names and seeing them
   scenario "allow players entering their names" do
+
     visit '/'
-    fill_in('player1', with: 'Iryna')
+    fill_in('player1', with: 'David')
     fill_in('player2', with: 'Barry')
     click_button('Submit')
-    expect(page).to have_content("Iryna vs. Barry")
+    expect(page).to have_content("David vs. Barry")
+  end
+
+  # As Player 1,
+  # So I can see how close I am to winning
+  # I want to see Player 2's Hit Points
+  scenario "allows user to see player 2's hitpoints" do
+    # firefox
+    visit '/'
+    fill_in('player1', with: 'David')
+    fill_in('player2', with: 'Barry')
+    click_button('Submit')
+    expect(page).to have_content("Barry: 60/60 HP")
   end
 end
