e51a18197af9d161c1bc69642bc81a942d7d852c
David Parry <drjparry@gmail.com>
2016-02-05 15:12:32 +0100

    Implemented multiplayer functionality
    As Player 1,
    So I can see how close I am to losing,
    I want to see my own hit points
    
    As Player 1,
    So I can lose a game of Battle,
    I want Player 2 to attack me, and I want to get a confirmation
    
    As Player 1,
    So I can start to lose a game of Battle,
    I want Player 2's attack to reduce my HP

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,7 @@
 require 'forwardable'
 
 class Game
-  
+
   attr_reader :current, :opponent
 
   extend Forwardable
@@ -9,6 +9,7 @@ class Game
     def_delegator :@player_1, :name, :name1
     def_delegator :@player_2, :name, :name2
     def_delegator :@player_2, :hit_points, :hit_points2
+    def_delegator :@player_1, :hit_points, :hit_points1
 
     attr_reader :player_1, :player_2
 
@@ -22,14 +23,14 @@ class Game
   def attack(player)
     player.receive_damage
   end
-  
+
   def switch_turns
     self.current.name == self.player_1.name ? (self.current = player_2) : (self.current = player_1)
     self.opponent.name == self.player_2.name ? (self.opponent = player_1) : (self.opponent = player_2)
   end
-  
-  private 
-  
+
+  private
+
   attr_writer :current, :opponent
 
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -13,26 +13,39 @@ feature "User Story => Battle" do
   # So I can see how close I am to winning
   # I want to see Player 2's Hit Points
   scenario "allows user to see player 2's hitpoints" do
-    # firefox
     sign_in_and_play
     expect(page).to have_content("Marco: 60 HP")
+  end
+
+  scenario "allows user to see player 1's hitpoints" do
+    sign_in_and_play
+    expect(page).to have_content("Dave: 60 HP")
+
   end
 
   # As Player 1,
   # So I can win a game of Battle,
   # I want to attack Player 2, and I want to get a confirmation
 
-  scenario 'allow user to see confirmation of attack' do
+  scenario 'allow user 1 to see confirmation of attack' do
     sign_in_and_play
     click_link('Attack')
     expect(page).to have_content("Dave attacked Marco")
+  end
+
+  scenario 'allow user 2 to see confirmation of attack' do
+    sign_in_and_play
+    click_link('Attack')
+    click_link('Switch Turn')
+    click_link('Attack')
+    expect(page).to have_content("Marco attacked Dave")
   end
 
   # As Player 1,
   # So I can start to win a game of Battle,
   # I want my attack to reduce Player 2's HP
 
-  scenario 'allows an attack to reduce players HP' do
+  scenario 'allows an attack to reduce player 1s HP' do
     sign_in_and_play
     click_link('Attack')
     click_link('Switch Turn')
@@ -41,5 +54,15 @@ feature "User Story => Battle" do
 
   end
 
+   scenario 'allows an attack to reduce player 2s HP' do
+    sign_in_and_play
+    click_link('Attack')
+    click_link('Switch Turn')
+    click_link('Attack')
+    click_link('Switch Turn')
+    expect(page).not_to have_content("Dave: 60 HP")
+    expect(page).to have_content("Dave: 50 HP")
+
+  end
 
 end
